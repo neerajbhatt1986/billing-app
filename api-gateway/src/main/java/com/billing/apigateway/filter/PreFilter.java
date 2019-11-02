f0af45ee94b459eed544a8ee8b9b4c037509a4a3
Niraj Bhatt <niraj.bhatt@synechron.com>
2019-11-02 07:40:37 +0530

    Updating the kubernaties configuration file for api-gateway

---
+++ api-gateway/src/main/java/com/billing/apigateway/filter/PreFilter.java
@@ -0,0 +1,33 @@
+package com.billing.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PreFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("Pre filter called");
+        return null;
+    }
+}
